bd48dbe00a252bd995d45320d413411718a48e17
Doug Watson <dougwatson@gocoder.io>
2023-01-31 16:47:59 -0600

    wotking first main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
-func main(){
+
+import "runtime"
+
+func main() {
 	println("Start by clicking Hello, 世界 in the window on the left")
-	println()
-	println("Click Run to get this message (it runs entirely in your web browser!)")
+
+	println("(", runtime.Version(), "- runs entirely in your web browser!)")
 }
