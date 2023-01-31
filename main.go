8de663bd2272100fccb25b23a32b820a17731242
Doug Watson <dougwatson@gocoder.io>
2023-01-31 16:33:42 -0600

    first main.go

---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+func main(){
+	println("Start by clicking Hello, 世界 in the window on the left")
+	println()
+	println("Click Run to get this message (it runs entirely in your web browser!)")
+}
