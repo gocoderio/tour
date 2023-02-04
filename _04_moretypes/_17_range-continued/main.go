d1abe883f2da954df1602bc07909de857fc4859e
Alex Watson <alexwatson@Alexs-MacBook-Pro.local>
2023-02-04 10:44:45 -0600

    more readmes working

--- _04_moretypes/_17_range-continued/main.go
+++ _04_moretypes/_17_range-continued/main.go
@@ -1,19 +1,15 @@
-
+// +build OMIT
 
 package main
 
 import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m map[string]Vertex
-
 func main() {
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
 	}
-	fmt.Println(m["Bell Labs"])
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
 }
